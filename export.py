b8cb94f2235149e9064fed64602e39e04d748918
matyasczuczor <matyas.czuczor@gmail.com>
2019-02-07 18:41:33 +0100

    "done with export functions, but without asking for an input and repeated new blank lines"

--- export.py
+++ export.py
@@ -1,2 +1,21 @@
 
 # Export functions
+
+from reports import *
+
+with open('export_file.txt', "w") as opened_file:
+    opened_file.write(str(count_games("game_stat.txt")))
+    opened_file.write("\n")
+    opened_file.write(str(decide("game_stat.txt", 2000)))
+    opened_file.write("\n")
+    opened_file.write(str(get_latest("game_stat.txt")))
+    opened_file.write("\n")
+    opened_file.write(str(count_by_genre("game_stat.txt", "First-person shooter")))
+    opened_file.write("\n")
+    opened_file.write(str(get_line_number_by_title("game_stat.txt", "Counter-Strike")))
+    opened_file.write("\n")
+    opened_file.write(str(sort_abc("game_stat.txt")))
+    opened_file.write("\n")
+    opened_file.write(str(get_genres("game_stat.txt")))
+    opened_file.write("\n")
+    opened_file.write(str(when_was_top_sold_fps("game_stat.txt")))
