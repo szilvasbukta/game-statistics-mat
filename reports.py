f07c9aaa5b7703931cf7f29e9d9af18d0a477377
matyasczuczor <matyas.czuczor@gmail.com>
2019-02-07 13:38:50 +0100

    'done with two more functions. three tests is ok.'

--- reports.py
+++ reports.py
@@ -8,3 +8,27 @@ def count_games(file_name):
         for line in opened_file:
             counter += 1
         return counter
+
+
+def decide(file_name, year):
+    with open(file_name, 'r') as opened_file:
+        check = 0
+        for line in opened_file:
+            line = line.split("\t")
+            if int(line[2]) == year:
+                check = 1
+        if check == 1:
+            return True
+        else:
+            return False
+
+
+def get_latest(file_name):
+    basic_year = ['', 0]
+    with open(file_name, 'r') as opened_file:
+        for line in opened_file:
+            line = line.split("\t")
+            if int(line[2]) > basic_year[1]:
+                basic_year[1] = int(line[2])
+                basic_year[0] = line[0]
+        return basic_year[0]
